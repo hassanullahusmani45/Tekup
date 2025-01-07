9c527c7adceafee77eef13ea28d42049d7010dd9
hassanullahusmani45 <hassanullahusmani45@gmail.com>
2025-01-07 23:00:57 +0430

    Perf: add the prop-types functionality in Layout page

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import propTypes from 'prop-types'
 
 export default function Layout({ children }) {
     return (
         <>
             <Header />
-            <div className='w-full px-6 pt-32  h-[50rem] bg-slate-900 text-white'>
+            <div className='w-full px-6 pt-32 bg-slate-900 text-white'>
                 {children}
             </div>
             <Footer />
         </>
     )
+}
+
+Layout.propTypes = {
+    children: propTypes.node.isRequired,
 }

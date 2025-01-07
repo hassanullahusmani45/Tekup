c2fb1d8115cda4a0420b504c5fe8a1de0aaad78f
hassanullahusmani45 <hassanullahusmani45@gmail.com>
2025-01-07 23:30:48 +0430

    Feat : codeing of the logout logic for apiand Installation of Seetalert and it cutomization in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,66 @@
-import { useRoutes } from "react-router-dom";
+import { useNavigate, useRoutes } from "react-router-dom";
 import routes from "./routes";
 import { useCallback, useEffect, useState } from "react";
 import AuthContext from "./context/AuthContext";
 import axios from "./api/axios";
+import swal from "sweetalert";
+import './App.css';
 
 function App() {
   const router = useRoutes(routes);
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [token, setToken] = useState(null);
   const [userInformation, setUserInformation] = useState(null);
 
   const login = useCallback(
-    (token,userInfo) => {
+    (token, userInfo) => {
       setIsLoggedIn(true);
       setToken(token);
       setUserInformation(userInfo);
       localStorage.setItem("token", JSON.stringify(token));
-    },[]);
+      navigate("/");
+    }, []);
 
-  const logout = () => {
-    setIsLoggedIn(false);
-    setToken(null);
-    setUserInformation(null);
-    localStorage.removeItem("token");
-  };
-
-  useEffect(() => { 
+  const logout = useCallback(() => {
     const token = JSON.parse(localStorage.getItem("token"));
-    axios.post("/verify-token",{
+
+    axios.post("/logout", { token })
+      .then((response) => {
+        if (response.data.status === "success") {
+          setIsLoggedIn(false);
+          setToken(null);
+          setUserInformation(null);
+          localStorage.removeItem("token");
+          navigate("/");
+          console.log("Logged out successfully");
+
+        } else {
+          setError(response.data.error);
+          console.log("error", error);
+          swal({
+            title: response.data.error,
+            icon: "error",
+            button: "Ok"
+          })
+        }
+
+      })
+      .catch((error) => {
+        console.log(error.response || "Error during logout");
+      });
+  }, [navigate]);
+
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    if (!token) {
+      return;
+    }
+    axios.post("/verify-token", {
       token
     }).then(response => {
       const status = response.data.status;
@@ -37,13 +69,13 @@ function App() {
       }
       if (status === "error") {
         logout();
-      }      
+      }
     }).catch(error => {
       console.log(error.response);
     })
   }, []);
 
-  
+
   return (
     <AuthContext.Provider
       value={{

865fd5091b9b621024d2549a71e70ec738e49288
hassanullahusmani45 <hassanullahusmani45@gmail.com>
2025-01-12 14:00:25 +0430

    Feat: add the landingConter component and it is logic code

---
+++ src/components/LandingCounter.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { PlusIcon } from "@heroicons/react/24/outline";
+
+export default function LandingCounter({ count }) {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => {
+        if (prevCounter + 1 >= count) {
+          clearInterval(interval); // پاک کردن تایمر وقتی مقدار به حد مورد نظر برسد
+          return count;
+        }
+        return prevCounter + 1;
+      });
+    }, 10);
+
+    return () => clearInterval(interval);
+  }, [count]);
+
+  return (
+    <div className="flex justify-center items-center gap-1">
+      <div className="text-xl font-medium text-center text-slate-400">{counter.toLocaleString()}</div> 
+      <PlusIcon className='size-6 text-slate-200' />
+    </div>
+  );
+}
+
+LandingCounter.propTypes = {
+  count: PropTypes.number.isRequired,
+};
